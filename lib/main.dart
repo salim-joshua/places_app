bc60e17bbb134854e9415ce52c7eb548f03e023e
Salim Joshua <salimslisch@gmail.com>
2020-01-24 11:14:13 +0100

    added add_place_screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import './providers/places_provider.dart';
+import './screens/add_place_screen.dart';
 import './screens/places_list_screen.dart';
 
 void main() => runApp(MyApp());
@@ -18,6 +19,9 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.amber,
         ),
         home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
       ),
     );
   }
---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a new place!'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text('User Inputs'),
+          RaisedButton.icon(
+            icon: Icon(Icons.add),
+            label: Text('ADD PLACE'),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
